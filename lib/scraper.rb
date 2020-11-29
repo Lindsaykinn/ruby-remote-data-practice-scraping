af3688315af685ecc1e686141257a5ddea988882
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-29 11:44:56 -0500

    commit tree 23e3e96b608ba777630dba83743259d084f8ce7f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".title-oE5vT4")[0].attributes
+
+
+# binding.pry
+
+
